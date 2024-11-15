1f9c619b4d0933f99eab188e7d552f4b0063dc53
Nayan Bhatt <134206855+freaky4wrld@users.noreply.github.com>
2024-11-15 18:02:32 +0000

    refactor: shifted to interfaces

--- backend/Services/match-service/src/index.ts
+++ backend/Services/match-service/src/index.ts
@@ -1,5 +1,5 @@
-import { User } from "./User";
+import { User } from "./interfaces/User";
 
-let a: User = new User("Bobby", 12);
+let a: User = {uuid: 224, name: "Bobby", age: 24, ans: 12, location: "pune" };
 
-console.log(`User is ${a.name}, and weight is ${a.calculateWeight()}`);
+console.log(`User is ${a.name}, age is ${a.age}, location is ${a.location}`);
---
+++ backend/Services/match-service/src/interfaces/User.ts
@@ -0,0 +1,7 @@
+export interface User{
+    readonly uuid : number
+    name : string
+    age : number
+    ans: number
+    location : string
+}
