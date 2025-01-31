ffd55d900c461ba79a8e95de9a687a40df2b88e6
freaky4wrld <freaky4wrld@gmail.com>
2025-01-31 14:58:18 +0530

    feat: entities added, except user

---
+++ backend/src/main/java/app/backend/entity/Task.java
@@ -0,0 +1,54 @@
+package app.backend.entity;
+
+import java.security.Timestamp;
+import java.util.List;
+
+import app.backend.enums.StatusEnum;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Task {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String title;
+	private String description;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusEnum status;
+	
+//	@ManyToOne
+//	@JoinColumn(name = "assigned_to")
+//	private User assignedTo;
+	
+	@ManyToOne
+	@JoinColumn(name = "board_id")
+	private Board board;
+	
+	private Timestamp createdAt;
+	private Timestamp updatedAt;
+	
+	@OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+	private List<Comment> comments;
+}
