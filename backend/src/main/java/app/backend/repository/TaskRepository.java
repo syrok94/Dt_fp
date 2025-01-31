31d7aadc0a6ab5bbaa304c378a0f6201c22f363d
freaky4wrld <freaky4wrld@gmail.com>
2025-01-31 15:07:15 +0530

    added repositories for the entity

--- backend/src/main/java/app/backend/repository/TaskRepository.java
+++ backend/src/main/java/app/backend/repository/TaskRepository.java
@@ -3,8 +3,8 @@ package app.backend.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import app.backend.entity.Board;
+import app.backend.entity.Task;
 @Repository
-public interface BoardRepository extends JpaRepository<Board, Long>{
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
