bac98261c5802f164f67a54aab57e19aa29bc7de
Anuvind Paul <anuvindpl05@gmail.com>
2025-02-01 18:08:28 +0530

    Revert "changes made to security auth request and userInfoDetails"
    This reverts commit 9ec1242df778fd590a6a732c3d5cf6b6440c1298.

--- backend/src/main/java/app/backend/entity/AuthRequest.java
+++ backend/src/main/java/app/backend/entity/AuthRequest.java
@@ -1,35 +1,27 @@
 package app.backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthRequest {
 
-	private String email;
-	private String password;
-
-	public AuthRequest() {
-		super();
+    public String getUsername() {
+		return username;
 	}
-
-	public AuthRequest(String email, String password) {
-		super();
-
-		this.email = email;
-		this.password = password;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setUsername(String email) {
-		this.email = email;
-	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	private String username;
+    private String password;
 
 }
