cd7d4069c24437cdd11946e9cc46e32b3dac5665
Bobby <bobbypatel94@gmail.com>
2025-02-02 12:26:11 +0530

    added cors

--- backend/src/main/java/app/backend/controller/UserController.java
+++ backend/src/main/java/app/backend/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import app.backend.service.JwtService;
 import app.backend.service.UserInfoService;
 
 @RestController
+@CrossOrigin("http://localhost:5173/")
 @RequestMapping("/auth")
 public class UserController {
 
