379d69ab74283a111d63db31fcc14da271b3e596
freaky4wrld <freaky4wrld@gmail.com>
2025-02-05 13:06:20 +0530

    feat: added upadate password funcationality

--- backend/src/main/java/app/backend/service/OTPServiceImpl.java
+++ backend/src/main/java/app/backend/service/OTPServiceImpl.java
@@ -5,21 +5,35 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import app.backend.entity.UserInfo;
+import app.backend.repository.UserInfoRepository;
 @Service
 public class OTPServiceImpl implements OTPService {
 	
 	@Autowired
 	RedisTemplate<String, String> redis;
+	
+	@Autowired
+	EmailService emailService;
+	
+	@Autowired
+	UserInfoRepository userRepo;
+	
+	@Autowired
+	PasswordEncoder encoder;
 
 	@Override
 	public String generateOTP(String email) {
 		// TODO Auto-generated method stub
-		int expirationTime = 60*1000;
+		int expirationTime = 5*60*1000;
 		SecureRandom secureRandom = new SecureRandom();
 		String otp = String.valueOf(secureRandom.nextInt(1000,9999));
 		redis.opsForValue().set(email, otp,expirationTime,TimeUnit.MILLISECONDS);
-		return otp;
+		emailService.sendOTPEmail(email, otp);
+		return "OTP send successfully";
 	}
 
 	@Override
@@ -31,6 +45,15 @@ public class OTPServiceImpl implements OTPService {
 			return true;
 		}
 		return false;
+	}
+	
+	@Override
+	public String updatePassword(String email, String password) {
+		// TODO Auto-generated method stub
+		UserInfo user = userRepo.findByEmail(email).get();
+		user.setPassword(encoder.encode(password));
+		userRepo.save(user);
+		return "Password updated successfully";
 	}
 
 }
